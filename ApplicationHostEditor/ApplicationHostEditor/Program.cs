0199833e35a653bcce52246a11b31653edee7b96
Dylan Smith <dylan.smith888@gmail.com>
2017-11-13 15:29:54 +0200

    Changes

--- ApplicationHostEditor/ApplicationHostEditor/Program.cs
+++ ApplicationHostEditor/ApplicationHostEditor/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Web.Administration;
+using Microsoft.Web.Management;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
 
 namespace ApplicationHostEditor
 {
@@ -10,6 +12,36 @@ namespace ApplicationHostEditor
     {
         static void Main(string[] args)
         {
+            using (ServerManager manager = new ServerManager())
+            {
+                var config = manager.GetApplicationHostConfiguration();
+
+                try
+                {
+                    ConfigurationSection accessSection = config.GetSection("system.webServer/security/access", "Default Web Site/WebInterface");
+
+                    if (accessSection != null)
+                    {
+                        Console.WriteLine("Opened app host config successfully.");
+                        accessSection["sslFlags"] = @"Ssl";
+                    }
+
+                    accessSection = config.GetSection("system.webServer/security/access", "Default Web Site/WebInterface/AttorneyCompliance.svc");
+
+                    if (accessSection != null)
+                    {
+                        accessSection["sslFlags"] = @"Ssl, SslNegotiateCert, SslRequireCert";
+                    }
+
+                    manager.CommitChanges();
+                    Console.WriteLine("Successfully committed changes");
+                    Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to open the app host config.");
+                }
+            }
         }
     }
 }
